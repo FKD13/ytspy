1609eb7076e8533cac09c90cdd7c47cdb469dec6
FKD13 <francisklinck@gmail.com>
2019-08-26 20:10:25 +0200

    disable video, add option to enable it

--- player.sh
+++ player.sh
@@ -6,8 +6,15 @@ then
   mkfifo music.fifo
 fi
 
+# Enable video playback
+VIDEO="--no-vid"
+if [[ "$1" == "--video" ]]
+then
+  VIDEO=""
+fi
+
 # Create a new mpv process that runs in backgound
-mpv --force-window=yes --input-file=music.fifo --idle > mpv.log 2>&1 &
+mpv $VIDEO --force-window=yes --input-file=music.fifo --idle > mpv.log 2>&1 &
 
 # Grab the mpv process so we can kill it later
 PROCESS=$(ps | grep 'mpv' | sed -E 's/^ *([0-9]+) .*$/\1/')
